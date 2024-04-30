1051d2e5d9387513662bfc0e600dda1f03cdac5c
vivekolladapu328@gmail.com <vivekolladapu>
2024-04-30 16:27:07 +1000

    Socket task

--- mvc_model/server.js
+++ mvc_model/server.js
@@ -4,7 +4,8 @@ const path = require('path');
 const cardController = require('./controllers/CardController');
 
 const app = express();
-const port = 3000;
+// const port = 3000;
+let http = require('http').createServer(app);
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -16,6 +17,28 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views','index.html'));
 });
 
-app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
-});
+// app.listen(port, () => {
+//     console.log(`Server running on http://localhost:${port}`);
+// });
+
+let io = require('socket.io')(http);
+http.listen(3000,
+    () => {
+        console.log('express server started');
+    });
+
+io.on('connection', (socket) => {
+    console.log('User connected');
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+    setInterval(() => {
+        socket.emit('number', parseInt(Math.random() * 10));
+    }, 1000);
+    socket.on('number', (msg) => {
+        console.log('Random number: ' + msg);
+    })
+})
+
+module.exports = app;
+
